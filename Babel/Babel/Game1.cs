1e7f9bc540510f86064eac8a18e138d7836af52e
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2021-03-06 15:04:01 -0800

    Project Finished

--- Babel/Babel/Game1.cs
+++ Babel/Babel/Game1.cs
@@ -2,89 +2,115 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace Babel
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        KeyboardState oldkb;
+        Enum gameState;
+        Enum language;
+        int posInPhases = 0;
+        String[] statePhases = new String[3];
+        // Dictionary<string, string> English = new Dictionary<string, string>();
+        Dictionary<string, string> English;
+        Dictionary<string, string> Spanish;
+        Dictionary<string, string> German;
+        String currentLanguage = "English";
+        Texture2D tempTexture;
+        string[] lines;
+        SpriteFont font;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
+            English = new Dictionary<string, string>();
+            Spanish = new Dictionary<string, string>();
+            German = new Dictionary<string, string>();
             // TODO: Add your initialization logic here
+            lines = System.IO.File.ReadAllLines(@"Content/languagetext.txt");
 
+             for (int x=0;x<14;x=x+5) {
+            English.Add(lines[x],lines[x+1]);
+            Spanish.Add(lines[x], lines[x + 2]);
+            German.Add(lines[x], lines[x + 3]);
+            }
+            statePhases[0] = "English";
+            statePhases[1] = "Spanish";
+            statePhases[2] = "German";
+            oldkb = Keyboard.GetState();
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            tempTexture = Content.Load<Texture2D>("bluesquare");
+            font = Content.Load<SpriteFont>("SpriteFont1");
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-
+            KeyboardState kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.D1)) {
+                 currentLanguage = statePhases[0];
+            }
+            else if (kb.IsKeyDown(Keys.D2))
+            {
+                currentLanguage = statePhases[1];
+            }
+            else if (kb.IsKeyDown(Keys.D3))
+            {
+                currentLanguage = statePhases[2];
+            }
             // TODO: Add your update logic here
-
+            oldkb = kb;
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            String string1 = "";
+            String string2 = "";
+            String string3 = "";
+            switch (currentLanguage) {
+                case "English":
+                    string1 = English["start"];
+                    string2 = English["save"];
+                    string3 = English["done"];
+                    break;
+                case "Spanish":
+                    string1 = Spanish["start"];
+                    string2 = Spanish["save"];
+                    string3 = Spanish["done"];
+                    break;
+                case "German":
+                    string1 = German["start"];
+                    string2 = German["save"];
+                    string3 = German["done"];
+                    break;
+            }
+            spriteBatch.DrawString(font,string1 , new Vector2(50, 20), Color.White);
+            spriteBatch.DrawString(font, string2, new Vector2(50, 60), Color.White);
+            spriteBatch.DrawString(font, string3, new Vector2(50, 100), Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
